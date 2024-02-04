327985bd9fad35c35e54c6163f825f94fa3b91f1
simon <simonesders2003@gmail.com>
2024-02-04 16:37:29 +0100

    login style

--- src/app/user-access/login/login.component.ts
+++ src/app/user-access/login/login.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
 import {HeaderComponent} from "../header/header.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  remember: boolean = false;
+
+  /**
+   * Toggles remember
+   * */
+  toggleRemember(): void {
+    this.remember = !this.remember;
+  }
 
 }
